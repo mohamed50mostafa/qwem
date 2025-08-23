ad396694fbdde8c0113f851bca6f216d8f94a865
mohamed50mostafa <mohamedmostafakheder@outlook.com>
2025-08-23 13:21:14 +0300

    kk2

--- project/settings.py
+++ project/settings.py
@@ -97,10 +97,10 @@ TEMPLATES = [
 # DATABASE
 # -------------------------------------------------------------
 DATABASES = {
-    'default': dj_database_url.config(
-        default=os.getenv("DATABASE_URL", "sqlite:///db.sqlite3"),
-        conn_max_age=600
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 # -------------------------------------------------------------
 # PASSWORD VALIDATION
